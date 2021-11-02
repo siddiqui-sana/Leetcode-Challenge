0f63fd47974ada1d8a467a1131e5cd49fab21074
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-02 22:34:13 +0530

    Delete Satyam/DAY - 1 directory

--- Satyam/DAY - 1/Counting_Bits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        res = 0
-        res = [0]
-        for i in range(1, n+1):
-            res.append(res[i//2]+(i % 2))
-        return res
