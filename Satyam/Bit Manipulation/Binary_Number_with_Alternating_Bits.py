6180c2518bb086fe6800134b0150cc5e4989a014
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-02 22:44:41 +0530

    Delete Satyam/Bit Manipulation directory

--- Satyam/Bit Manipulation/Binary_Number_with_Alternating_Bits.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasAlternatingBits(self, n: int) -> bool:
-        while n > 1:
-            bit1 = n & 1
-            n >>= 1
-            bit2 = n & 1
-            if bit1 == bit2:
-                return False
-        return True
--- Satyam/Bit Manipulation/Reverse_Bits.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        res = 0
-        for i in range(31, -1, -1):
-            if n & 1:
-                res |= (1 << i)
-            n = n >> 1
-        return res
