5e4afb0369c795cb8c79539a1f908026bdabd94d
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-10 13:03:44 +0530

    Update Contains_Duplicate.py

--- Leetcode Challenge/Elite/Contains_Duplicate.py
+++ Leetcode Challenge/Elite/Contains_Duplicate.py
@@ -1,8 +1,17 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        # Time and Space: O(n)
-        hashSet = set()
-        for i in range(len(nums)):
-            if nums[i] in hashSet:
-                return True
-            hashSet.add(nums[i])
+	def containsDuplicate(self, nums: List[int]) -> bool:
+		# Time and Space: O(n)
+		# 1st Approach:
+		hashSet = set()
+		for i in range(len(nums)):
+			if nums[i] in hashSet:
+				return True
+			hashSet.add(nums[i])
+
+		# 2nd Approach: 
+		hashMap = dict()
+		for i in range(len(nums)):
+			if nums[i] in hashMap:
+				return True
+			else:
+				hashMap[i] = 1
