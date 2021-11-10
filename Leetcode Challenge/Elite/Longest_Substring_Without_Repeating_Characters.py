fc349a0e23ea29426b0dbecf35a6f115fdd45db4
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-11 02:06:27 +0530

    Update Longest_Substring_Without_Repeating_Characters.py

--- Leetcode Challenge/Elite/Longest_Substring_Without_Repeating_Characters.py
+++ Leetcode Challenge/Elite/Longest_Substring_Without_Repeating_Characters.py
@@ -1,6 +1,7 @@
+# 1st Approach:
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        maxL = -(1 << 31)
+        maxL = 0
         winS = 0
         hashMap = {}
         for winE in range(len(s)):
@@ -9,4 +10,26 @@ class Solution:
                 winS = max(winS, hashMap[char]+1)
             hashMap[char] = winE
             maxL = max(maxL, winE-winS+1)
+        return maxL
+
+# 2nd Approach:
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        winS = winE = 0
+        hashMap = {}
+        maxL = 0
+        while winE < len(s):
+            hashMap[s[winE]] = hashMap.get(s[winE], 0)+1
+            if len(hashMap) == winE-winS+1:
+                maxL = max(maxL, winE-winS+1)
+                winE += 1
+            else:
+                if hashMap[s[winS]] == 1:
+                    del hashMap[s[winS]]
+                    winS += 1
+                    winE += 1
+                else:
+                    hashMap[s[winS]] -= 1
+                    winS += 1
+                    winE += 1
         return maxL
