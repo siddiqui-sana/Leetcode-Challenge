2b6f49d11aa122c89c6f5bf8c8f633a963d51fd3
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-12 01:32:28 +0530

    Update Max_Consecutive_Ones_III.py

--- Leetcode Challenge/Elite/Max_Consecutive_Ones_III.py
+++ Leetcode Challenge/Elite/Max_Consecutive_Ones_III.py
@@ -1,14 +1,39 @@
+# 1st Approach:
 class Solution:
     def longestOnes(self, arr: List[int], k: int) -> int:
-        maxL = -(1 << 31)
+        maxL = -(1 << 31)  # -2147482348
+        ones_count = 0
+        winS = 0
+        for winE in range(len(arr)):
+            if arr[winE] == 1:
+                ones_count += 1
+            if (winE-winS+1)-ones_count > k:
+                if arr[winS] == 1:
+                    ones_count -= 1
+                winS += 1
+            maxL = max(maxL, (winE-winS+1))
+        return maxL
+
+# 2nd Approach:
+class Solution:
+    def longestOnes(self, arr: List[int], k: int) -> int:
+        winS = 0
+        winE = 0
+        hashMap = {}
         ones = 0
-        ws = 0
-        for we in range(len(arr)):
-            if arr[we] == 1:
-                ones += 1
-            if (((we-ws+1)-(ones)) > k):
-                if arr[ws] == 1:
-                    ones -= 1
-                ws += 1
-            maxL = max(maxL, we-ws+1)
+        maxL = 0
+        while winE < len(arr):
+            hashMap[arr[winE]] = hashMap.get(arr[winE], 0)+1
+            if winE-winS+1-hashMap.get(1, 0) <= k:
+                maxL = max(maxL, winE-winS+1)
+                winE += 1
+            else:
+                if hashMap[arr[winS]] == 1:
+                    del hashMap[arr[winS]]
+                    winS += 1
+                    winE += 1
+                else:
+                    hashMap[arr[winS]] -= 1
+                    winS += 1
+                    winE += 1
         return maxL
