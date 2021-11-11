f6770b58db2b86ad8ad8b420bd5a5e5a1b33df6b
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-12 02:25:10 +0530

    Update Longest_Repeating_Character_Replacement.py

--- Leetcode Challenge/Elite/Longest_Repeating_Character_Replacement.py
+++ Leetcode Challenge/Elite/Longest_Repeating_Character_Replacement.py
@@ -1,3 +1,35 @@
+# 1st Approach:
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        winS = 0
+        winE = 0
+        hashMap = {}
+        maxL = 0
+
+        def solve(hashMap):
+            value = hashMap.values()
+            return max(value)
+        while winE < len(s):
+            hashMap[s[winE]] = hashMap.get(s[winE], 0)+1
+            if len(hashMap) == 1:
+                maxL = max(maxL, winE-winS+1)
+                winE += 1
+            else:
+                if winE-winS+1-solve(hashMap) <= k:
+                    maxL = max(maxL, winE-winS+1)
+                    winE += 1
+                else:
+                    if hashMap[s[winS]] == 1:
+                        del hashMap[s[winS]]
+                        winS += 1
+                        winE += 1
+                    else:
+                        hashMap[s[winS]] -= 1
+                        winS += 1
+                        winE += 1
+        return maxL
+
+# 2nd Approach:
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         winS = 0
@@ -14,4 +46,35 @@ class Solution:
                     freq[s[winS]] -= 1
                 winS += 1
             maxL = max(maxL, winE-winS+1)
+        return maxL
+
+# 3rd Approach:
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        winS = 0
+        winE = 0
+        hashMap = {}
+        maxL = 0
+
+        def solve(hashMap):
+            res = sorted(hashMap.values(), reverse=True)
+            return sum(res[1:])
+        while winE < len(s):
+            hashMap[s[winE]] = hashMap.get(s[winE], 0)+1
+            if len(hashMap) == 1:
+                maxL = max(maxL, winE-winS+1)
+                winE += 1
+            else:
+                if solve(hashMap) <= k:
+                    maxL = max(maxL, winE-winS+1)
+                    winE += 1
+                else:
+                    if hashMap[s[winS]] == 1:
+                        del hashMap[s[winS]]
+                        winS += 1
+                        winE += 1
+                    else:
+                        hashMap[s[winS]] -= 1
+                        winS += 1
+                        winE += 1
         return maxL
