97e587d7f1357f2a5bd8d90b5eaab2318d42f80f
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-12 01:25:25 +0530

    Update Add_Binary.py

--- Leetcode Challenge/Bit Manipulation/Easy/Add_Binary.py
+++ Leetcode Challenge/Bit Manipulation/Easy/Add_Binary.py
@@ -1,3 +1,4 @@
+# 1st Approach:
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         i = len(a)-1
@@ -18,3 +19,19 @@ class Solution:
         if carry != 0:
             ans.append(carry)
         return "".join(map(str, ans[::-1]))
+
+# 2nd Approach:
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = list(a)
+        b = list(b)
+        carry = 0
+        res = ""
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+            res += str(carry % 2)
+            carry //= 2
+        return res[::-1]
