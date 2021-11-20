c0573979ef73781d7512de95ef3f9c5a29292c0d
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-20 14:03:11 +0530

    Delete Leetcode Challenge/Elite directory

--- Leetcode Challenge/Elite/Minimum_Value_to_Get_Positive_Step_by_Step_Sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minStartValue(self, nums: List[int]) -> int:
-        ans = 0
-        sum = 0
-        for i in range(len(nums)):
-            sum += nums[i]
-            ans = min(ans, sum)
-        return -ans+1
