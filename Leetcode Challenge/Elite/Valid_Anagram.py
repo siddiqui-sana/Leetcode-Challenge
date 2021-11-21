c74f3c3a31daeb0576cd07bc145941dc196c2894
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-11-21 09:48:50 +0530

    Delete Leetcode Challenge/Elite directory

--- Leetcode Challenge/Elite/Valid_Anagram.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        res = [0]*26
-        for i in range(len(s)):
-            res[ord(s[i])-ord('a')] += 1
-            res[ord(t[i])-ord('a')] -= 1
-        return all(num == 0 for num in res)
